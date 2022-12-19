f2d6faaeb3c54dd8135725b77db4d42eae52bebb
aravindh-c <61825537+aravindh-c@users.noreply.github.com>
2022-12-19 07:57:26 +0530

    Update batch_prediciton.py

--- batch_prediciton.py
+++ batch_prediciton.py
@@ -8,6 +8,7 @@ if __name__=="__main__":
      try:
           #start_training_pipeline()
           output_file = start_batch_prediction(input_file_path=file_path)
+          logging.info(f"Prediction file :{output_file}")
           print(output_file)
      except Exception as e:
           print(e)
